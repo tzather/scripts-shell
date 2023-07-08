ba5d0cec5edc9bdefe89644a31c0982063a263c3
tanvir <tzather@gmail.com>
2023-07-07 22:31:22 -0600

    remove db scripts

--- database/Database.sql
+++
@@ -1,9 +0,0 @@
--- Create a new database called 'Azure'
--- Connect to the 'master' database to run this snippet
-USE master
-GO
-
--- Create the new database if it does not exist already
-IF NOT EXISTS (SELECT [name] FROM sys.databases WHERE [name] = N'Azure')
-  CREATE DATABASE Azure
-GO
